50478d5bd76e17a3c677be2a8345ce2a2ca419fb
ebdet2002 <adarsh.r.ramakrishnan@gmail.com>
2020-05-11 16:03:03 -0400

    created skeleton

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// This is the container for the actual game itself. 
+// In other words, this is where we set up the interface and read
+// user inputs/interactions. 
+
+const gameElement = document.querySelector('#main-game')
+
+const render = () => {
+    // render a blank game to the gameElement
+    gameElement.textContent = ''
+    
+    // display the puzzle letter by letter
+    //  
+}
