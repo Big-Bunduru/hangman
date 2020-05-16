3f52c203374b10bd9ec238e71fd3096db4e2b3ca
ebdet2002 <adarsh.r.ramakrishnan@gmail.com>
2020-05-16 18:57:29 -0400

    cleaned up app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,29 @@
-// This is the container for the actual game itself. 
-// In other words, this is where we set up the interface and read
-// user inputs/interactions. 
+var keyword = prompt("Enter: Hangman word");
+var len = keyword.length;
+var lives = 7
+//console.log(len)
 
-const gameElement = document.querySelector('#main-game')
-
-const render = () => {
-    // render a blank game to the gameElement
-    gameElement.textContent = ''
-    
-    // display the puzzle letter by letter
-    //  
+for (i = 0; i < len; i++) {
+    $('body').append("<div class='div'></div>").css('color', 'black');
+    //console.log(keyword[i])
+    //console.log(keyword.charCodeAt(i))
 }
+
+
+$('body').keypress(function(event){ 
+    var success = false;
+    //console.log(event.keyCode)
+    if (lives > 0) {
+        for (i = 0; i < len; i++) {
+            if(event.keyCode == keyword.charCodeAt(i)){
+                $("body").find("div").eq(i).text(keyword[i]);
+                success = true;
+            }
+            //console.log(keyword.charCodeAt(i))
+        }
+        if (!success) {
+            lives--;
+            $("header").text("Lives remaining: " + lives);
+        }
+    }
+ })
