9e1fada1eb23bb7fcf43f6d9d27f5c56a720e92c
ebdet2002 <adarsh.r.ramakrishnan@gmail.com>
2020-05-16 19:29:49 -0400

    updates to app.js, merged with requests

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,21 @@
-var word_count;
+const wordCount = '5'
 
-$("button").click(function(){
-    word_count = prompt("Enter word count:");
-});
+const getPuzzle = async (wordCount) => {
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    
+    if (response.status === 200) {
+        const puzzle  = await response.json()
+        return puzzle.puzzle
+    } else {
+        throw new Error('Unable to fetch puzzle')
+    }
+}
 
-//var keyword = prompt("Enter: Hangman word");
-//var len = keyword.length;
-//var lives = 7
-//console.log(len)
+const puzzle = getPuzzle(wordCount)
+
+var len = puzzle.length
+var lives = 7
+console.log(len)
 
 for (i = 0; i < len; i++) {
     $('body').append("<div class='div'></div>").css('color', 'black');
@@ -39,28 +47,3 @@ $('body').keypress(function(event){
 // This is the container for the actual game itself. 
 // In other words, this is where we set up the interface and read
 // user inputs/interactions. 
-
-for (i = 0; i < len; i++) {
-    $('body').append("<div class='div'></div>").css('color', 'black');
-    //console.log(keyword[i])
-    //console.log(keyword.charCodeAt(i))
-}
-
-
-$('body').keypress(function(event){ 
-    var success = false;
-    //console.log(event.keyCode)
-    if (lives > 0) {
-        for (i = 0; i < len; i++) {
-            if(event.keyCode == keyword.charCodeAt(i)){
-                $("body").find("div").eq(i).text(keyword[i]);
-                success = true;
-            }
-            //console.log(keyword.charCodeAt(i))
-        }
-        if (!success) {
-            lives--;
-            $("header").text("Lives remaining: " + lives);
-        }
-    }
- })
