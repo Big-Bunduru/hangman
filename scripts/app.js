c55da9f46ac3c2ed17e4ee3ec88e0a95e13efc1d
ebdet2002 <adarsh.r.ramakrishnan@gmail.com>
2020-05-24 08:05:46 -0400

    split dem words

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,38 @@
 function game() {
-    var word_count = prompt("Enter word count:");
-    const getPuzzle = async (word_count) => {
-        const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${word_count}`)
+    const wordCount = prompt("Enter word count:");
+    const getPuzzle = async (wordCount) => {
+        const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
         
         if (response.status === 200) {
             const data  = await response.json()
-            var puzzle_string = await data.puzzle
-            console.log(puzzle_string);
-            var len = puzzle_string.length;
-            for (i = 0; i < len; i++) {
-                $('body').append("<div class='div'></div>").css('color', 'black');
+            const puzzleString = await data.puzzle.toLowerCase()
+            console.log(puzzleString);
+            const len = puzzleString.length;
+            const words = puzzleString.split(' ')
+            const wordArray = Array.from(puzzleString)
+            console.log(words)
+            for (i = 0; i < words.length; i++) {
+                $('#main-game').append(`"<div class='word' id='word${i}'>"`)
             }
-            var lives = 7;
+            for (i = 0; i < len; i++) {
+                if (wordArray[i] === " ") {
+                    $('#main-game').append("<div class='divSpace'></div><br>")
+                } else {
+                    $('#main-game').append("<div class='div'></div>")
+                }
+            }
+            let lives = 7;
             $("header").text("Lives remaining: " + lives);
             $('body').keypress(function(event){ 
-                var success_flag = false;
+                var successFlag = false;
                 if (lives > 0) {
                     for (i = 0; i < len; i++) {
-                        if(event.keyCode == puzzle_string.charCodeAt(i)){
-                            $("body").find("div").eq(i).text(puzzle_string[i]);
+                        if(event.keyCode == puzzleString.charCodeAt(i)){
+                            $("body").find("div").eq(i).text(puzzleString[i]);
                             success_flag = true;
                         }
                     }
-                    if (!success_flag) {
+                    if (!successFlag) {
                         lives--;
                         $("header").text("Lives remaining: " + lives);
                     }
@@ -38,7 +48,7 @@ function game() {
             throw new Error('Unable to fetch puzzle')
         }
     }
-    getPuzzle(word_count)
+    getPuzzle(wordCount)
 }
 
 $("button").click(function(){
